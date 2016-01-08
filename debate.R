820d91ec1336052647a275674aee243a053225cd
pstessel <pstessel@gmail.com>
2016-01-08 18:29:25 -0500

    trump lines

--- debate.R
+++ debate.R
@@ -2,27 +2,47 @@
 rm(list=ls(all=T))
 
 # set_working_directory
-setwd("~/repos/debates")
+setwd("~/repos/debates/texts")
 getwd()
 
-debate <- scan("r_121515.txt", what = "character", sep = "\n")
+debate.v <- scan("r_121515.txt", what="character", sep="\n")
 
-speakers <- debate[grepl("^[^\\s]\\w+:", debate)]
+gsub("(^.*)(:){1}(.+\n\n)", "", debate.v)
+?gsub
 
-speaker_split <- strsplit(speakers, split = ":")
-speaker_split
+debate.1 <- grep("(.+\n\n)*", "", debate.v)
+debate.1[[1]]
 
-speaker_names <- sapply(speaker_split, "[", 1L)
-speaker_names
+# trim leading and trailing spaces from the lines
+debate.v <- stri_trim(debate.v)
 
-speaker_parts <- sapply(speaker_split, function(x) paste(x[-1L], collapse = ":"))
-speaker_parts
+# get rid of empty lines
+debate.v <- debate.v[nzchar(debate.v)]
+debate.v
 
-trump <- which(speaker_names == "TRUMP")
+# remove stage directions
+stage_dir_rx <- exactly(
+  OPEN_BRACKET %R%
+    one_or_more(printable()) %R%
+    "]"
+)
+is_stage_dir_line <- stri_detect_regex(debate.v, stage_dir_rx)
+debate.v <- debate.v[!is_stage_dir_line]
+is_stage_dir_line
 
-trump_words <- unlist(strsplit(speaker_parts[trump], split = " "))
-trump_words
+# match lines containing "character.dialogue"
+character_dialogue_rx <- START %R%
+  optional(capture(one_or_more(alpha()) %R% lookahead(":"))) %R%
+  optional(":") %R%
+  zero_or_more(space()) %R%
+  capture(one_or_more(printable()))
+matches <- stri_match_first_regex(debate.v, character_dialogue_rx)
+character_dialogue_rx
 
-x <- c("abc", "def", "cba a", "aa")
-m <- regexpr("a+", x, perl = T)
-m
+# store the matches in a data.table (needed for the roll functionality);
+# a line number key column is also needed shortly
+debate_data <- data.table(
+  line_number = seq_len(nrow(matches)),
+  character = matches[,2],
+  dialogue = matches[,3]
+)
--- debates2.R
+++ debates2.R
@@ -1,17 +1,55 @@
-setwd("/Volumes/HD2/Users/pstessel/Documents/Git_Repos/debates")
+setwd("C:/Users/pstessel/Documents/repos/debates/texts/")
+
+require(dplyr)
 
 rm(list=ls(all = TRUE))
 
 # Read in text
-debate.v <- scan("texts/r_121515.1.txt", what="character", sep="\n")
+debate.v <- scan("r_121515.1.txt", what="character", sep="\n")
 debate.v
+
+
+debate.df <- data.frame(do.call('rbind', strsplit(as.character(debate.v),':',fixed=TRUE)))
+
+trump_lines <- debate.df[which(debate.df$X1=="TRUMP"),]
+trump_lines$X1 <- NULL
+trump_lines$X2 <- as.character(trump_lines$X2)
+class(trump_lines)
+trump_lines.v <- as.vector(trump_lines)
+trump_lines.l <- as.list(trump_lines)
+
+trump.lines.lower.v <- tolower(trump_lines.v)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class(new_table)
+new_table <- rename(new_table, c(V1="speaker", V2="lines"))
+
+trump_lines <- subset(new_table, speaker="TRUMP", select="lines")
 
 # Pull out the spoken lines
 speakers <- debate.v[grepl("^[^\\s]\\w+:", debate.v)]
 speakers
 
 # Split off the speaker's names
-speaker_split <- strsplit(speakers, split = ":")
+speaker_split <- debate.v(speakers, split = ":")
 speaker_split
 
 # Get names
@@ -26,3 +64,6 @@ speaker_parts
 trump <- which(speaker_names == "TRUMP")
 
 (unlist(strsplit(speaker_parts[trump], split = "")) == " ")
+
+
+write.csv(new_table, file = "new_table.csv")
