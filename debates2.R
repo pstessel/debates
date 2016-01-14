5f722f728c0ed98c46302b2b4f8e02ce0a2ac9f4
pstessel <pstessel@gmail.com>
2016-01-14 18:26:05 -0500

    revisions to debates2

--- debates2.R
+++ debates2.R
@@ -1,65 +1,77 @@
 setwd("C:/Users/pstessel/Documents/repos/debates/texts/")
 
-require(dplyr)
-
 rm(list=ls(all = TRUE))
+
+require(dplyr)
+require(stringr)
 
 # Read in text
 debate.v <- scan("r_121515.1.txt", what="character", sep="\n")
-debate.v
-
 
 debate.df <- data.frame(do.call('rbind', strsplit(as.character(debate.v),':',fixed=TRUE)))
+class(debate.df)
 
 trump_lines <- debate.df[which(debate.df$X1=="TRUMP"),]
 trump_lines$X1 <- NULL
-trump_lines
+trump_lines.v <- unlist(trump_lines)
+trump_lines.v <- as.character(trump_lines.v)
+class(trump_lines.v)
+trump_lines.v
 
+#### Jockers, p. 15 ####
 
+# Convert text to lowercase
+trump.lower.v <- tolower(trump_lines.v)
 
+# Split words into a list at spaces
+trump.words.l <- strsplit(trump.lower.v, "\\W")
+trump.words.l
 
+# Convert list to a vector
+trump.words.v <- unlist(trump.words.l)
 
+# Figure out which items in the vector are not blank
+not.blanks.v <- which(trump.words.v !="")
+not.blanks.v
 
+# Retain only non-blank items
+trump.words.v <- trump.words.v[not.blanks.v]
+trump.words.v
 
+--------------------------------------------
 
+# Convert vector of character strings to a corpus of text using 'VectorSource' and 'Corpus'
+require(tm)
+txt <- VectorSource(trump_lines)
+txt.corpus <- Corpus(txt); rm(txt)
+inspect(txt.corpus)
 
+txt.corpus <- tm_map(txt.corpus, tolower)
+inspect(txt.corpus)
+txt.corpus <- tm_map(txt.corpus, removePunctuation)
+txt.corpus <- tm_map(txt.corpus, removeNumbers)
+txt.corpus <- tm_map(txt.corpus, removeWords, stopwords("english"))
 
+# Stem to truncate words
+require(SnowballC)
+txt.corpus <- tm_map(txt.corpus, stemDocument)
+detach("package:SnowballC")
+inspect(txt.corpus)
 
+# Remove empty spaces
 
+txt.corpus <- tm_map(txt.corpus, stripWhitespace)
+inspect(txt.corpus)
 
+txt.corpus <- tm_map(txt.corpus, PlainTextDocument)
 
+tdm <- TermDocumentMatrix(txt.corpus)
+#inspect(tdm)
+inspect(tdm)
 
+terms <- inspect(tdm)
+terms
+terms <- order(terms)
+terms
 
-
-
-
-
-
-class(new_table)
-new_table <- rename(new_table, c(V1="speaker", V2="lines"))
-
-trump_lines <- subset(new_table, speaker="TRUMP", select="lines")
-
-# Pull out the spoken lines
-speakers <- debate.v[grepl("^[^\\s]\\w+:", debate.v)]
-speakers
-
-# Split off the speaker's names
-speaker_split <- debate.v(speakers, split = ":")
-speaker_split
-
-# Get names
-speaker_names <- sapply(speaker_split, "[", 1L)
-speaker_names
-
-# And what they said (collapsing because their lines may have had other colons that we lost):
-speaker_parts <- sapply(speaker_split, function(x) paste(x[-1L], collapse = ":"))
-unlist(speaker_parts)
-speaker_parts
-
-trump <- which(speaker_names == "TRUMP")
-
-(unlist(strsplit(speaker_parts[trump], split = "")) == " ")
-
-
-write.csv(new_table, file = "new_table.csv")
+findFreqTerms(x=tdm, lowfreq=8, highfreq=Inf)
